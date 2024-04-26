43038d967e995721eeccfcd764cd5889cf739faf
Gabor Ballabas <gbr666@gmail.com>
2024-04-26 18:50:51 +0200

    Add error handling

--- ch06/types.go
+++ ch06/types.go
@@ -260,3 +260,80 @@ func (ack *Ack) UnmarshalBinary(packet []byte) error {
 
     return binary.Read(packetReader, binary.BigEndian, ack)
 }
+
+
+// Error packet structure
+//
+// # 2 bytes # 2 bytes # n bytes # 1 byte #
+// ########################################
+// # OpCode  # ErrCode # Message #   0    #
+// ########################################
+
+type TFTPError struct {
+    Error ErrCode
+    Message string
+}
+
+func (tftpErr TFTPError) MarshalBinary() ([]byte, error) {
+    // operation code + error code + message + 0 byte
+    packetSize := 2 + 2 + len(tftpErr.Message) + 1
+
+    buf := new(bytes.Buffer)
+    buf.Grow(packetSize)
+
+    // write operation code
+    err := binary.Write(buf, binary.BigEndian, OpErr)
+    if err != nil {
+	return nil, err
+    }
+
+    // write error code
+    err = binary.Write(buf, binary.BigEndian, tftpErr.Error)
+    if err != nil {
+	return nil, err
+    }
+
+    // write message
+    _, err = buf.WriteString(tftpErr.Message)
+    if err != nil {
+	return nil, err
+    }
+
+    // write 0 byte
+    err = buf.WriteByte(0)
+    if err != nil {
+	return nil, err
+    }
+
+    return buf.Bytes(), nil
+}
+
+func (tftpErr *TFTPError) UnmarshalBinary(packet []byte) error {
+    packetReader := bytes.NewBuffer(packet)
+
+    var code OpCode
+
+    // read operation code
+    err := binary.Read(packetReader, binary.BigEndian, &code)
+    if err != nil {
+	return err
+    }
+
+    if code != OpErr {
+	return errors.New("invalid ERROR")
+    }
+
+    // read error code
+    err = binary.Read(packetReader, binary.BigEndian, &tftpErr.Error)
+    if err != nil {
+	return err
+    }
+
+    // read error message
+    tftpErr.Message, err = packetReader.ReadString(0)
+
+    // remove the 0-byte
+    tftpErr.Message = strings.TrimRight(tftpErr.Message, "\x00")
+
+    return err
+}
